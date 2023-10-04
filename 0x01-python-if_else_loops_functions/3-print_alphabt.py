7920b20788afa63dbc3d5d63701c77013c73e546
emmyajaks <emmyajaks2016@gmail.com>
2023-10-04 07:26:09 -0700

    updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+output = ''
 for char in range(ord('a'), ord('z') + 1):
     if char != ord('q') and char != ord('e'):
-        print(chr(char), end='')
+        output += chr(char)
+
+print(output, end='')
