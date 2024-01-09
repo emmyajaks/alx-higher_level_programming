f323f5347596435a045a6b4a95c3677e9c119365
emmyajaks <emmyajaks2016@gmail.com>
2024-01-09 06:03:48 -0800

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+// Basic print with Javascript
+
 console.log('C is fun');
 console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log('Javascript is amazing');
