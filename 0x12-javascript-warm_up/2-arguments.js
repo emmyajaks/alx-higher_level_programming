c778e77a5c79e50976d9633d224b2ffc7ec356d3
emmyajaks <emmyajaks2016@gmail.com>
2024-01-09 06:05:43 -0800

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+// Handle arguments with Javascript
+
+if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
